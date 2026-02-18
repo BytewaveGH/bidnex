9a96ef068a822b8b9dd4541b901ee9485b268d04
Nuache <michaelopoku790@gmail.com>
2026-02-18 14:35:44 +0000

    changes for logo

--- components/templates/logo.tsx
+++ components/templates/logo.tsx
@@ -2,12 +2,16 @@
 import React from 'react'
 import { cn } from '@/lib/utils'
 import { useRouter } from 'next/navigation'
+import Image from 'next/image'
+import logo from '@/assets/images/logo.png'
 
 export default function Logo({ className }: { className?: string }) {
   const router = useRouter()
   return (
     <div
     onClick={() => router.push('/')}
-    className={cn("text-3xl font-semibold shrink-0 hover:cursor-pointer " , className)}>BIDNEX</div>
+    className={cn("text-3xl font-semibold shrink-0 hover:cursor-pointer flex gap-1" , className)}>
+        <Image src={logo} alt="logo" width={33} height={33} />
+      BIDCHALE</div>
   )
 }
